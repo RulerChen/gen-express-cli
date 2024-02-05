ae434172b45bdeb09ff8196c7d10e4f1561672d5
Josh <joshtu0627@gmail.com>
2024-02-05 13:21:47 +0800

    chores: delete redundant example apis

--- packages/templates/javascript/sequelize/src/routes/user.routes.js
+++ packages/templates/javascript/sequelize/src/routes/user.routes.js
@@ -6,12 +6,4 @@ const router = express.Router();
 
 router.post('/', UserController.createUser);
 
-router.get('/', UserController.getAllUsers);
-
-router.get('/:id', UserController.getUserById);
-
-router.put('/:id', UserController.updateUserById);
-
-router.delete('/:id', UserController.deleteUserById);
-
 export default router;
--- packages/templates/javascript/sequelize/src/services/user.service.js
+++ packages/templates/javascript/sequelize/src/services/user.service.js
@@ -4,26 +4,6 @@ async function createUser(username, password) {
   return await User.create({ username, password });
 }
 
-async function getAllUsers() {
-  return await User.findAll();
-}
-
-async function getUserById(id) {
-  return await User.findByPk(id);
-}
-
-async function updateUserById(id, username, password) {
-  return await User.update({ username, password }, { where: { id } });
-}
-
-async function deleteUserById(id) {
-  return await User.destroy({ where: { id } });
-}
-
 export default {
   createUser,
-  getAllUsers,
-  getUserById,
-  updateUserById,
-  deleteUserById,
 };
